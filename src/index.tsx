515d5dedd101b1cce41ffd6e4a0816030afffff1
Artsiom Marozau <artsiom_marozau@epam.com>
2023-10-25 19:52:09 +0200

    feat: integrate FE

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
   },
 });
 
-if (import.meta.env.DEV) {
+if (import.meta.env.MOCK) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
